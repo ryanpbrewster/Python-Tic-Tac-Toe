68e77e863475bc801cdf6c4e3966f2a2738ebbb3
SeanCC <sean.ch.carter@gmail.com>
2013-05-16 16:46:25 -0400

    Fixed GUI to work with changes to TicTacToe.py (now using TicTacToeAI.py and TicTacToeBoard.py)

--- TicTacToeGUI.py
+++ TicTacToeGUI.py
@@ -1,5 +1,6 @@
 from tkinter import *
-from TicTacToe import *
+from TicTacToeBoard import *
+from TicTacToeAI import *
 import math
 
 class TTTGame(Frame):
@@ -10,12 +11,15 @@ class TTTGame(Frame):
 		self.canvasWidth = self.width - 100
 		self.canvasHeight = self.height - 100
 		root.geometry(str(self.width)+"x"+str(self.height))
-		self.gameBoard = TicTacToeBoard()
-		self.createGame()
+		
 		self.numPlayers = 1
 		self.turn = 0
 		self.gameOver = False
-	
+		self.TTTAI = TicTacToeAI('O')
+		self.gameBoard = TicTacToeBoard()
+		
+		self.createGame()
+		
 	def createGame(self):
 		self.columnconfigure(0, pad = 5)
 		self.columnconfigure(1, pad = 5)
@@ -35,7 +39,57 @@ class TTTGame(Frame):
 		self.pack()
 
 		self.createRectangles()
-
+	
+	def setPlayers(self):
+		tempNum = int(self.numPlayersField.get())
+		if tempNum == 1 or tempNum == 2:
+			self.numPlayers = int(self.numPlayersField.get())
+	def createRectangles(self):
+		rectangleWidth = (self.canvasHeight / 3)
+		rectangleHeight = (self.canvasHeight / 3)
+		index = 0
+		for p in range(3):
+			for i in range(3):
+				currentRect = self.gameCanvas.create_rectangle(i * rectangleWidth, p*rectangleHeight, (i * rectangleWidth) + rectangleWidth, (p * rectangleHeight) + rectangleHeight, fill = "black", tag=str(index), outline = 'white')
+				self.gameCanvas.tag_bind(currentRect, "<ButtonRelease-1>", self.tacClick)
+				index = index + 1
+				
+	def tacClick(self, event):
+		# Only works if game is not over
+		if self.gameOver == False:
+			# Determines which rectangle was clicked and gets tac position from that
+			rect = self.gameCanvas.find_closest(event.x, event.y)
+			tacPosition = int(self.gameCanvas.gettags(rect)[0])
+			placed = False
+			# Determines where to put tac based on turn, then lets TicTacToe methods handle the rest and calls updateCanvas
+			if self.turn == 0:
+				placed = self.gameBoard.putTac('X', tacPosition + 1 )
+			else:
+				placed = self.gameBoard.putTac('O', tacPosition + 1 )
+			if placed == True:
+				self.updateCanvas()
+				if self.turn == 1:
+					self.turn = 0
+				else:
+					self.turn = 1
+				if self.numPlayers == 1:
+					self.gameBotMove()
+					
+	def gameBotMove(self):
+		self.TTTAI.botMove(self.gameBoard)
+		self.updateCanvas()
+		self.turn = 0
+		
+	def newGame(self):
+		self.gameCanvas.delete(ALL)
+		self.gameBoard = TicTacToeBoard()
+		self.turn = 0
+		self.gameOver = False
+		self.createGame()
+		
+	def quit(self):
+		root.quit()
+		
 	def updateCanvas(self):
 		# Checks each board position on TicTacToe board, then sets cooresponding canvas rectangle
 		for i in range(9):
@@ -57,59 +111,7 @@ class TTTGame(Frame):
 		elif self.gameBoard.checkFull():
 			self.gameCanvas.create_text((math.floor(self.canvasWidth/4), math.floor(self.canvasHeight / 5)), fill = 'white', text="Game Draw!")
 			self.gameOver = True
-	# Sets players based on player number field at top
-	def setPlayers(self):
-		tempNum = int(self.numPlayersField.get())
-		if tempNum == 1 or tempNum == 2:
-			self.numPlayers = int(self.numPlayersField.get())
-	# Creates the rectangles composing the game board
-	def createRectangles(self):
-		rectangleWidth = (self.canvasHeight / 3)
-		rectangleHeight = (self.canvasHeight / 3)
-		index = 0
-		for p in range(3):
-			for i in range(3):
-				currentRect = self.gameCanvas.create_rectangle(i * rectangleWidth, p*rectangleHeight, (i * rectangleWidth) + rectangleWidth, (p * rectangleHeight) + rectangleHeight, fill = "black", tag=str(index), outline = 'white')
-				self.gameCanvas.tag_bind(currentRect, "<ButtonRelease-1>", self.tacClick)
-				index = index + 1
-
-	def tacClick(self, event):
-		# Only works if game is not over
-		if self.gameOver == False:
-			# Determines which rectangle was clicked and gets tac position from that
-			rect = self.gameCanvas.find_closest(event.x, event.y)
-			tacPosition = int(self.gameCanvas.gettags(rect)[0])
-			placed = False
-			# Determines where to put tac based on turn, then lets TicTacToe methods handle the rest and calls updateCanvas
-			if self.turn == 0:
-				placed = self.gameBoard.putTac('X', tacPosition + 1 )
-			else:
-				placed = self.gameBoard.putTac('O', tacPosition + 1 )
-			if placed == True:
-				self.updateCanvas()
-				if self.turn == 1:
-					self.turn = 0
-				else:
-					self.turn = 1
-				if self.numPlayers == 1:
-					self.gameBotMove()
-
-	# This method is basically a wrapper for the TicTacToe AI that also updates the canvas
-	def gameBotMove(self):
-		self.gameBoard.botMove('O')
-		self.updateCanvas()
-		self.turn = 0
-	# Clears canvas, resets board, resets turn, resets gameOver, creates game
-	def newGame(self):
-		self.gameCanvas.delete(ALL)
-		self.gameBoard = TicTacToeBoard()
-		self.turn = 0
-		self.gameOver = False
-		self.createGame()
-	def quit(self):
-		root.quit()
 		
-
 root = Tk()
 root.wm_title("Play TicTacToe!")
 TTT = TTTGame(root)
